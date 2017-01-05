7fdfef75c529f743fa6042297abf91750cd5bc7e
Sam Mason <samm@lostmy.name>
2017-01-05 18:04:07 +0000

    Remove all of redux code and use internal state

--- src/Beers.js
+++ src/Beers.js
@@ -1,17 +1,9 @@
 import React from 'react'
-import { connect } from 'react-redux'
 import Beer from './Beer'
-
-const mapStateToProps = function (state) {
-  return {
-    data: state.beerData.data
-  }
-}
 
 const Beers = (props) => (
   <div className="mt4 flex-l flex-wrap">
     { props.data.map((p, i) => <Beer {...p} key={p.id} />) }
   </div>
 )
-
-export default connect(mapStateToProps)(Beers)
+export default Beers
--- src/SearchField.js
+++ src/SearchField.js
@@ -1,22 +1,27 @@
 import React from 'react'
 
-const SearchField = (props) => (
-  <div>
-    <form
-      className="flex mt4"
-      onSubmit={props.handleSubmit}>
-      <input
-        className="bt-0 br-0 bl-0 bb bw3 f2 fw1 pv2 ph2 flex-auto b--bd-blue"
-        type="text"
-        onChange={props.handleChange}
-        value={props.searchFieldValue}
-      />
-      <button
-        className="bn bg-bd-blue white v-btm f3 fw1 tracked ttu pv2 ph3 disabled cursor-pointer">
-        Search
-      </button>
-    </form>
-  </div>
-)
+const SearchField = (props) => {
+  const handleOnChange = (e) => props.updateSearchValue(e.target.value)
+  const handleOnSubmit = (e) => props.submitAndFetch(e)
+
+  return (
+    <div>
+      <form
+        className="flex mt4"
+        onSubmit={handleOnSubmit}>
+        <input
+          className="bt-0 br-0 bl-0 bb bw3 f2 fw1 pv2 ph2 flex-auto b--bd-blue"
+          type="text"
+          onChange={handleOnChange}
+          value={props.searchFieldValue}
+        />
+        <button
+          className="bn bg-bd-blue white v-btm f3 fw1 tracked ttu pv2 ph3 disabled cursor-pointer">
+          Search
+        </button>
+      </form>
+    </div>
+  )
+}
 
 export default SearchField
