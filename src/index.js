c4b78f31367bc55153f75510821f7d5938580481
Sam Mason <samm@lostmy.name>
2017-01-05 22:26:37 +0000

    Wrap devtools in conditional

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import Inferno from 'inferno'
 import App from './App';
-import 'inferno-devtools'
 import 'tachyons';
 import './styles.css'
+
+if(process.env.NODE_ENV !== 'production') { require('inferno-devtools') }
 
 Inferno.render(
   <App />,
